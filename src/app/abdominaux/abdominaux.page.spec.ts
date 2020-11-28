04a9fe2d69d713960a7e3e2456163244e5ee722a
NourStudent <achournourelhouda2@gmail.com>
2020-11-28 16:35:46 -0500

    first commit

---
+++ src/app/abdominaux/abdominaux.page.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA,} from '@angular/core';
+import { ABDOMINAUXPage } from './abdominaux.page';
+
+describe('ABDOMINAUXPage', () => {
+  let component: ABDOMINAUXPage;
+  let fixture: ComponentFixture<ABDOMINAUXPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ABDOMINAUXPage ],
+      imports: [IonicModule.forRoot()],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ABDOMINAUXPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/triceps/triceps.page.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA,} from '@angular/core';
+import { TRICEPSPage } from './triceps.page';
+
+describe('TRICEPSPage', () => {
+  let component: TRICEPSPage;
+  let fixture: ComponentFixture<TRICEPSPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TRICEPSPage ],
+      imports: [IonicModule.forRoot()],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TRICEPSPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
